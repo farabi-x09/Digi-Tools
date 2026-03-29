31118946f94a895b94715c60b7af900337eebe04
farabi-x09 <farabiahmed2005@gmail.com>
2026-03-30 00:08:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Banner from './Components/Banner'
+import Navber from './Components/Navber'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navber></Navber>
+    <Banner></Banner>
+    </>
+  )
+}
+
+export default App
