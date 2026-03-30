40dfdcd0fb6c94c377570fc3b7368747877ea6e6
farabi-x09 <farabiahmed2005@gmail.com>
2026-03-30 09:44:04 +0600

    Count part compleat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Banner from './Components/Banner'
+import Count from './Components/Count'
 import Navber from './Components/Navber'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navber></Navber>
     <Banner></Banner>
+    <Count></Count>
     </>
   )
 }
