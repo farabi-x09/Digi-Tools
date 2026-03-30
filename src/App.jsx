3324bb8b4fa1660ccbfcde5ba33b23926e517ea6
farabi-x09 <farabiahmed2005@gmail.com>
2026-03-30 11:13:34 +0600

    2 btn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Banner from './Components/Banner'
+import Cart from './Components/Cart'
 import Count from './Components/Count'
 import Navber from './Components/Navber'
 
@@ -12,6 +13,7 @@ function App() {
     <Navber></Navber>
     <Banner></Banner>
     <Count></Count>
+    <Cart></Cart>
     </>
   )
 }
